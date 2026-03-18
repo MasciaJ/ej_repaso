6a6594e08b84aa9f664423cb232b16b511f402d0
sbudani-cloud <jmasciamarquez@alumno.huergo.edu.ar>
2026-03-18 11:43:45 -0300

    20.1

---
+++ ej20.1.py
@@ -0,0 +1,5 @@
+pedidos_sandwiches = ["sandwich de jamon", "sandwich de carne", "sandwich de queso"]
+sandwiches_terminados = []
+for sandwich in pedidos_sandwiches:
+    sandwiches_terminados.append(sandwich)
+    print(f"el {sandwich} esta listo")
